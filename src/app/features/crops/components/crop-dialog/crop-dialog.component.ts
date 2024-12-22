90fd3f476cede76a5c34b41999b34125ea06f3a6
Roberto Amador <josarroliga0904@gmail.com>
2024-12-22 12:24:04 -0600

    feat: scrollTo slider (needs to be fixed)

--- src/app/features/crops/components/crop-dialog/crop-dialog.component.ts
+++ src/app/features/crops/components/crop-dialog/crop-dialog.component.ts
@@ -1,14 +1,45 @@
-import { Component, input, InputSignal } from "@angular/core";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  input,
+  InputSignal,
+  OnDestroy,
+  OnInit,
+  viewChild,
+} from "@angular/core";
 import { Crop } from "../../../../core/models/crops.interface";
 import { CardComponent } from "../../../../shared/components/card/card.component";
 import { CropDetailsComponent } from "../crop-details/crop-details.component";
+import { CropIncomingComponent } from "../crop-incoming/crop-incoming.component";
 
 @Component({
   selector: "star-crop-dialog",
-  imports: [CardComponent, CropDetailsComponent],
+  imports: [CardComponent, CropDetailsComponent, CropIncomingComponent],
   templateUrl: "./crop-dialog.component.html",
   styleUrl: "./crop-dialog.component.css",
 })
 export class CropDialogComponent {
-  public crop: InputSignal<Crop> = input.required<Crop>();
+  private _sliderRef = viewChild<ElementRef<HTMLDivElement>>("sliderRef");
+
+  protected crop: InputSignal<Crop> = input.required<Crop>();
+
+  constructor() {}
+
+  protected onButtonScroll = (id: string): void => {
+    const slideToScroll: HTMLElement = document.getElementById(
+      id,
+    ) as HTMLElement;
+
+    if (slideToScroll && this._sliderRef()) {
+      const offSet: number =
+        slideToScroll.offsetLeft - this._sliderRef()!.nativeElement.offsetLeft;
+      const sliderRef = this._sliderRef()!.nativeElement;
+
+      sliderRef.scrollTo({
+        left: offSet,
+        behavior: "smooth",
+      });
+    }
+  };
 }
---
+++ src/app/features/crops/components/crop-incoming/crop-incoming.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'star-crop-incoming',
+  imports: [],
+  templateUrl: './crop-incoming.component.html',
+  styleUrl: './crop-incoming.component.css'
+})
+export class CropIncomingComponent {
+
+}
